3f7d5b6beddcd0f1afc806619b56388f57649875
qorrnsmj <qorrnsmj@gmail.com>
2024-11-27 08:20:43 +0900

    refactor: Some refactorings

--- src/main/kotlin/qorrnsmj/smf/game/entity/custom/WagonEntity.kt
+++ src/main/kotlin/qorrnsmj/smf/game/entity/custom/WagonEntity.kt
@@ -1,8 +1,6 @@
 package qorrnsmj.smf.game.entity.custom
 
 import qorrnsmj.smf.game.entity.Entity
-import qorrnsmj.smf.game.entity.Models
-import qorrnsmj.smf.game.entity.component.Model
 
 class WagonEntity : Entity() {
 }
