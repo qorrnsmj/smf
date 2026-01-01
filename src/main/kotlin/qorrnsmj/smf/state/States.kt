1c10b4edbbdd65893ada41bd094b58ec25cdb329
qorrnsmj <qorrnsmj@gmail.com>
2026-01-01 12:05:42 +0900

    fix: Delete unused files

--- src/main/kotlin/qorrnsmj/smf/state/States.kt
+++ src/main/kotlin/qorrnsmj/smf/state/States.kt
@@ -2,7 +2,6 @@ package qorrnsmj.smf.state
 
 import qorrnsmj.smf.state.custom.EmptyState
 import qorrnsmj.smf.state.custom.TestState
-import qorrnsmj.smf.state.custom.SolarSystemState
 
 object States {
     val EMPTY = EmptyState()
